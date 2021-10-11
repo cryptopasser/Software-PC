1fab81e4b83b312ec5a0ded4eeeb25aa77d85053
Zusyaku <71582475+Zusyaku@users.noreply.github.com>
2021-10-11 22:20:20 +0700

    Add files via upload

---
+++ RAM Saver Professional 21.5.kuyhAa/_Unpack Portable.cmd
@@ -0,0 +1,19 @@
+@ECHO OFF
+color 0B
+mode con:cols=110 lines=15
+@cls
+echo.
+echo.
+echo.
+@echo     Бл          ллл   ллл ллл    ллл ллл       ллл ллл  ллл  ллллллллл  ллллллллл     
+@echo       л         ллл ллл   ллл    ллл   ллл   ллл   ллл  ллл лллллллллл лллллллллл     лллллллллллл ллллллл
+@echo        Вл       ллллллл   ллл    ллл     ллллл     лллллллл    ллллллл    ллллллл     лл   лл  ллл лл Аллл
+@echo       л         ллл ллл   ллл    ллл      ллл      ллл  ллл   лл   ллл   лл   ллл     лл   лл  ллл лллВ  
+@echo     Бл   ВВВВВ  ллл   ллл лллллллллл      ллл      ллл  ллл   лллллллл   лллллллл ллл лл   лл  ллл ллллллВ   
+@echo.  
+@echo                                      SILENT MODE... shh! Jika VerySilent Not Found, Abaikan !                        
+echo.                   
+@echo off
+FOR %%i IN ("RAM Saver*.exe") DO Set FileName="%%i"
+%FileName% /SILENT /PORTABLE=1
+@start VERYSILENT.url
