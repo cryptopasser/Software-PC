89e4d6a57a81f81d1109ea3f213f91b3e38fbcf0
Zusyaku <71582475+Zusyaku@users.noreply.github.com>
2021-10-11 22:33:54 +0700

    Add files via upload

---
+++ WColor Version-1.1 by MathInDOS/Help/Animation.bat
@@ -0,0 +1,27 @@
+@echo off
+If not exist wcolor.exe msg * please copy here wcolor.exe & exit
+wcolor $0!a0 "H"
+wcolor $0!b0 "e"
+wcolor $0!c0 "l"
+wcolor $0!d0 "l"
+wcolor $0!e0 "o"
+wcolor $0!f0 " W" REM SPACE is giver for space word
+wcolor $0!0a "o"
+wcolor $0!0b "r"
+wcolor $0!0c "l"
+wcolor $0!0d "d"
+wcolor $0!0e "!"
+echo. REM This define new line.
+echo.
+echo.
+wcolor $0!a0 " M"
+wcolor $0!b0 "a"
+wcolor $0!c0 "t"
+wcolor $0!d0 "h"
+wcolor $0!e0 "I"
+wcolor $0!0a "n"
+wcolor $0!0b "D"
+wcolor $0!0c "O"
+wcolor $0!0d "S"
+echo.
+Pause
